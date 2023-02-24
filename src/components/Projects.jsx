b3a021cd71fd24f12c95198787111b49019ec6f4
Shawn Aviles <shawnaviles26@gmail.com>
2023-02-23 22:48:48 -0500

    hero: bounce off logo

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,6 @@ function Projects() {
     <div style={{ backgroundColor: "#0078E8", padding: "250px" }}>
       <h1
         style={{
-          fontFamily: "Poppins, sans-serif",
           fontSize: "58px",
           color: "white",
           lineHeight: "87px",
@@ -15,7 +14,6 @@ function Projects() {
       </h1>
       <p
         style={{
-          fontFamily: "Poppins, sans-serif",
           fontSize: "20px",
           color: "white",
         }}
@@ -24,7 +22,6 @@ function Projects() {
       </p>
       <p
         style={{
-          fontFamily: "Poppins, sans-serif",
           fontSize: "20px",
           color: "white",
         }}
