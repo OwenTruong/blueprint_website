78c4fc5b25bd0e5aa364f2b0287879de7aac19e6
ChristianA <christiand.apostol@outlook.com>
2023-02-25 00:49:49 -0500

    useState on contact info

--- src/components/Apply.jsx
+++ src/components/Apply.jsx
@@ -1,22 +1,57 @@
+import { useState } from "react";
+
 function Apply() {
-  function handleSubmit() {
-    // console.log("Hello");
-  }
+  const [contactInfo, setContactInfo] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: Test inputs for valid stevens email
+    // TODO: Add Google Sheets API call
+    setContactInfo({
+      firstName: "",
+      lastName: "",
+      email: "",
+    });
+  };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <label htmlFor="firstName">
           First Name:
-          <input type="text" name="firstName" />
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            value={contactInfo.firstName}
+            onChange={(e) =>
+              setContactInfo({ ...contactInfo, firstName: e.target.value })
+            }
+          />
         </label>
         <label htmlFor="lastName">
           Last Name:
-          <input type="text" name="lastName" />
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+            value={contactInfo.lastName}
+            onChange={(e) => setContactInfo({ ...contactInfo, lastName: e.target.value })}
+          />
         </label>
         <label htmlFor="email">
           Email:
-          <input type="email" name="email" />
+          <input
+            type="email"
+            name="email"
+            placeholder="example@email.com"
+            value={contactInfo.email}
+            onChange={(e) => setContactInfo({ ...contactInfo, email: e.target.value })}
+          />
         </label>
         <input type="submit" value="Submit" />
       </form>
