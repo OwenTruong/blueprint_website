098a944f71529d4939823b14a0ad9f8b091173b3
ChristianA <christiand.apostol@outlook.com>
2023-02-25 01:11:06 -0500

    basic form styling with tailwind

--- src/components/Apply.jsx
+++ src/components/Apply.jsx
@@ -21,39 +21,63 @@ function Apply() {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="firstName">
-          First Name:
-          <input
-            type="text"
-            name="firstName"
-            placeholder="First Name"
-            value={contactInfo.firstName}
-            onChange={(e) =>
-              setContactInfo({ ...contactInfo, firstName: e.target.value })
-            }
-          />
-        </label>
-        <label htmlFor="lastName">
-          Last Name:
-          <input
-            type="text"
-            name="lastName"
-            placeholder="Last Name"
-            value={contactInfo.lastName}
-            onChange={(e) => setContactInfo({ ...contactInfo, lastName: e.target.value })}
-          />
-        </label>
-        <label htmlFor="email">
-          Email:
-          <input
-            type="email"
-            name="email"
-            placeholder="example@email.com"
-            value={contactInfo.email}
-            onChange={(e) => setContactInfo({ ...contactInfo, email: e.target.value })}
-          />
-        </label>
-        <input type="submit" value="Submit" />
+        <div className="overflow-hidden shadow sm:rounded-md">
+          <div className="bg-white px-4 py-5 sm:p-6">
+            <div className="grid grid-cols-6 gap-6">
+              <div className="col-span-6 sm:col-span-3">
+                <label
+                  htmlFor="firstName"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  First Name:
+                  <input
+                    type="text"
+                    name="firstName"
+                    placeholder="First Name"
+                    value={contactInfo.firstName}
+                    onChange={(e) =>
+                      setContactInfo({ ...contactInfo, firstName: e.target.value })
+                    }
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-lg focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </label>
+                <label
+                  htmlFor="lastName"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Last Name:
+                  <input
+                    type="text"
+                    name="lastName"
+                    placeholder="Last Name"
+                    value={contactInfo.lastName}
+                    onChange={(e) =>
+                      setContactInfo({ ...contactInfo, lastName: e.target.value })
+                    }
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-lg focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </label>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email:
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="example@email.com"
+                    value={contactInfo.email}
+                    onChange={(e) =>
+                      setContactInfo({ ...contactInfo, email: e.target.value })
+                    }
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-lg focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </label>
+                <input type="submit" value="Submit" />
+              </div>
+            </div>
+          </div>
+        </div>
       </form>
     </div>
   );
