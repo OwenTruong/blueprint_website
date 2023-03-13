d23dabdd598f43e2e6faad906109cda12cdd0312
Caposto <christiand.apostol@outlook.com>
2023-03-13 12:18:03 -0400

    added centering to Closed component

--- src/components/Applications/Closed.jsx
+++ src/components/Applications/Closed.jsx
@@ -2,7 +2,7 @@ function Closed() {
   // TODO: Prop drill recruiting cycled down from App.jsx
   const recruitingCycle = "Fall 2022 - Spring 2023";
   return (
-    <p className="text-2xl font-bold">
+    <p className="text-2xl font-bold grid h-screen place-items-center">
       Applications for {recruitingCycle} are now closed.{" "}
     </p>
   );
