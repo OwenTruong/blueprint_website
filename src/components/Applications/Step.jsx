35d56a54d67d57488ad1c434c49be1898f65e79d
Caposto <christiand.apostol@outlook.com>
2023-03-14 14:13:36 -0400

    iterated over basic components

--- src/components/Applications/Step.jsx
+++ src/components/Applications/Step.jsx
@@ -2,11 +2,11 @@ import PropTypes from "prop-types";
 
 function Step({ stepNumber, stepName, stepDescription }) {
   return (
-    <p>
-      {stepNumber}
-      {stepName}
-      {stepDescription}{" "}
-    </p>
+    <div className="grid grid-rows-3 grid-flow-col gap-4 px-32 py-4">
+      <div className="row-span-3 bg-secondary text-8xl">{stepNumber}</div>
+      <div className="col-span-2 bg-slate-500">{stepName}</div>
+      <div className="row-span-2 col-span-2 bg-slate-500">{stepDescription} </div>
+    </div>
   );
 }
 
--- src/components/Applications/StudentApply.jsx
+++ src/components/Applications/StudentApply.jsx
@@ -5,16 +5,41 @@ import Closed from "./Closed";
 import Step from "./Step";
 
 function StudentApply({ isOpen }) {
+  const applicationSteps = [
+    {
+      id: 1,
+      stepName: "Written Application",
+      stepDescription: "Submit your Resume and answer a few basic questions",
+    },
+    {
+      id: 2,
+      stepName: "Coffee Chats",
+      stepDescription:
+        "A 15-30 minute coffee chat with the Executive Board, which will provide us with an opportunity to learn more about you, your motivations, and your goals for joining Blueprint.",
+    },
+    {
+      id: 3,
+      stepName: "Project Assignment",
+      stepDescription:
+        "Project Assignment: For this semester we will be taking on 2-3 Non-Profit Organization (NPO) projects, for a total of around 2-3 project leads and 6-12 developers. Don't be disheartened if you're not selected for this first wave of projects!  We will be holding workshops to help prepare you for future projects and forming new teams for the next wave of NPOs in need.",
+    },
+  ];
+
+  const stepDescriptionBlocks = applicationSteps.map((step) => (
+    <Step
+      key={step.id}
+      stepNumber={step.id}
+      stepName={step.stepName}
+      stepDescription={step.stepDescription}
+    />
+  ));
+
   return (
     <div className="flex flex-col h-screen">
       <div className="mb-auto">
         <div>
-          <p>Developer Application Process</p>
-          <Step
-            stepNumber="1"
-            stepName="Written Application"
-            stepDescription="Submit your Resume and answer these questions"
-          />
+          <p className="text-4xl py-8 pl-4">Developer Application Process</p>
+          {stepDescriptionBlocks}
         </div>
         <div>{isOpen ? <Form /> : <Closed />}</div>
       </div>
