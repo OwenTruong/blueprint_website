5b4775a5d20a2d2f33e03445b876d5cf2a5b1d3c
Shawn Aviles <shawnaviles26@gmail.com>
2023-03-16 01:36:06 -0400

    working About Page with Footer

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,9 @@
-import Footer from "./Common/Footer";
-
 function About() {
   return (
     <div className="flex flex-col h-screen">
       <div className="mb-auto">
         <h1>Implement about page</h1>
       </div>
-      <Footer />
     </div>
   );
 }
