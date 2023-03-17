d5aa9c77ebaecbbd34cbeb4bc8dfd61f987228bc
Caposto <christiand.apostol@outlook.com>
2023-03-16 22:06:18 -0400

    finished base npo application page

--- src/components/Applications/NonprofitsApply.jsx
+++ src/components/Applications/NonprofitsApply.jsx
@@ -1,13 +1,36 @@
-import Footer from "../Common/Footer";
-// import Step from "./Step";
-// import FAQ from "./FAQ";
-// import content from "./NpoContent.json";
+import Step from "./Step";
+import FAQ from "./FAQ";
+import content from "./NpoContent.json";
 
 function Nonprofits() {
+  const APPLICATION_STEPS = content.npoSteps;
+  const FREQUENTLY_ASKED_QUESTIONS = content.frequentlyAskedQuestions;
+
+  const stepDescriptionBlocks = APPLICATION_STEPS.map((step) => (
+    <Step
+      key={step.id}
+      stepNumber={step.id}
+      stepName={step.stepName}
+      stepDescription={step.stepDescription}
+    />
+  ));
+
+  const faqBlocks = FREQUENTLY_ASKED_QUESTIONS.map((question) => (
+    <FAQ key={question.id} question={question.question} answer={question.answer} />
+  ));
+
+  // TODO: Add Sample Proposal to firebase server and link
   return (
-    <div className="flex flex-col h-screen">
-      <div className="mb-auto px-40">Non-Profits</div>
-      <Footer />
+    <div className="flex flex-col min-h-screen px-80">
+      <div>
+        <h1 className="text-4xl py-8">Non-Profit Application Process</h1>
+        <div>{stepDescriptionBlocks}</div>
+      </div>
+      <div>
+        <h1 className="text-4xl py-8">Frequently Asked Questions (FAQ)</h1>
+        <div>{faqBlocks}</div>
+      </div>
+      <div className="flex justify-center py-24"> Sample Proposal </div>
     </div>
   );
 }
