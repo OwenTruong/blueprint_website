be13d4d42b3aebd7a6f573bc931b3bb9e3aef2df
Caposto <christiand.apostol@outlook.com>
2023-03-17 17:08:31 -0400

    added link for email

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,16 @@
 function Contact() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen p-80">
       <div className="mb-auto">
-        <h1>Implement contact page</h1>
+        <h1 className="text-8xl text-primary">Stay in Touch</h1>
+        <div className="py-8">
+          <a
+            href="mailto:sit.blueprint@gmail.com"
+            className="text-2xl text-secondary underline py-8"
+          >
+            sit.blueprint@gmail.com
+          </a>
+        </div>
       </div>
     </div>
   );
