169b9f3595dcdfda8393aa6a3e8c62eade102fbb
Shawn Aviles <shawnaviles26@gmail.com>
2023-03-18 15:49:25 -0400

    base Project page layout

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,50 +1,86 @@
+import { Link } from "react-router-dom";
+import blueprintLogo from "../assets/logos/logo_negative.png";
+
 function Projects() {
   return (
     <div className="">
-      <div className="">
-        <div style={{ backgroundColor: "#0078E8", padding: "250px" }}>
-          <h1
-            style={{
-              fontSize: "58px",
-              color: "white",
-              lineHeight: "87px",
-              fontWeight: "bold",
-              marginTop: "-125px", // add negative margin to move the h1 element up
-            }}
-          >
-            Projects
-          </h1>
-          <p
-            style={{
-              fontSize: "20px",
-              color: "white",
-            }}
-          >
+      <div className="flex bg-primary py-24 px-10 md:px-52">
+        <div className="flex flex-col w-3/4">
+          <h1 className="text-6xl font-extrabold text-white pb-4">Projects</h1>
+          <p className="text-xl text-white">
             It&apos;s our priority to build projects that are both accessible and
             transparent.
           </p>
-          <p
-            style={{
-              fontSize: "20px",
-              color: "white",
-            }}
-          >
+          <p className="text-xl text-white">
             Feel free to browse some of our open-source solutions below.
           </p>
         </div>
-
-        <div style={{ backgroundColor: "white", padding: "250px" }}>
-          <h2
-            style={{
-              fontSize: "58px",
-              color: "#0078E8",
-              lineHeight: "87px",
-              fontWeight: "bold",
-              marginTop: "-125px", // add negative margin to move the h1 element up
-            }}
-          >
-            Project Showcase
-          </h2>
+        <div className="flex justify-center items-center w-1/4">
+          <img src={blueprintLogo} alt="Replace This!" className="h-32" />
+        </div>
+      </div>
+      <div className="bg-white py-20 px-10 md:px-52">
+        <h2 className="text-5xl font-extrabold text-primary text-center pb-10">
+          Project Showcase
+        </h2>
+        {/* <h3 className="text-4xl font-bold text-black text-center border-b-8 pb-4">
+          Current Projects
+        </h3> */}
+        <div className="flex justify-around py-6">
+          <div className="flex flex-colrelative w-80 bg-secondary border-4 border-primary rounded-3xl shadow-xl">
+            <div className="p-5">
+              <img className="p-10" src={blueprintLogo} alt="" />
+              <h5 className="rounded-3xl text-2xl text-black font-bold py-2">
+                Sample Project 1
+              </h5>
+              <p className="font-normal text-gray-700">
+                Here are the biggest enterprise technology acquisitions of 2021 so far, in
+                reverse order...
+              </p>
+              <Link
+                to="/projects"
+                className="flex justify-center p-2 text-lg text-blue-800 text-center"
+              >
+                Read More &gt;
+              </Link>
+            </div>
+          </div>
+          <div className="flex flex-colrelative w-80 bg-secondary border-4 border-primary rounded-3xl shadow-xl">
+            <div className="p-5">
+              <img className="p-10" src={blueprintLogo} alt="" />
+              <h5 className="rounded-3xl text-2xl text-black font-bold py-2">
+                Sample Project 2
+              </h5>
+              <p className="font-normal text-gray-700">
+                Here are the biggest enterprise technology acquisitions of 2021 so far, in
+                reverse order...
+              </p>
+              <Link
+                to="/projects"
+                className="flex justify-center p-2 text-lg text-blue-800 text-center"
+              >
+                Read More &gt;
+              </Link>
+            </div>
+          </div>
+          <div className="flex flex-colrelative w-80 bg-secondary border-4 border-primary rounded-3xl shadow-xl">
+            <div className="p-5">
+              <img className="p-10" src={blueprintLogo} alt="" />
+              <h5 className="rounded-3xl text-2xl text-black font-bold py-2">
+                Sample Project 3
+              </h5>
+              <p className="font-normal text-gray-700">
+                Here are the biggest enterprise technology acquisitions of 2021 so far, in
+                reverse order...
+              </p>
+              <Link
+                to="/projects"
+                className="flex justify-center p-2 text-lg text-blue-800 text-center"
+              >
+                Read More &gt;
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
