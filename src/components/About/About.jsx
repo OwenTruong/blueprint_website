23561288d5db3e3dca922065c66f37921e82d521
ChristianA <christiand.apostol@outlook.com>
2023-03-20 00:00:59 -0400

    added members to about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,18 +1,52 @@
+/* eslint-disable global-require */
 import Member from "./Member";
 import Mission from "./Mission";
+import Carousel from "../Common/Carousel";
+import Members from "./Members.json";
 
 function About() {
+  const carouselImages = {
+    resources: [
+      {
+        title: "Blueprint Test Image 1",
+        source: require("../../assets/dummyImage.png"),
+      },
+      {
+        title: "Blueprint Test Image 2",
+        source: require("../../assets/dummyImage.png"),
+      },
+      {
+        title: "Blueprint Test Image 3",
+        source: require("../../assets/dummyImage.png"),
+      },
+      {
+        title: "Blueprint Test Image 4",
+        source: require("../../assets/dummyImage.png"),
+      },
+    ],
+  };
+
+  const MEMBERS = Members.members;
+  const memberComponents = MEMBERS.map((mem) => (
+    <Member
+      key={mem.id}
+      name={mem.name}
+      position={mem.position}
+      linkedin={mem.linkedin}
+    />
+  ));
+
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col min-h-screen">
       <div className="mb-auto">
         <div className="flex justify-center py-16">
           <Mission />
         </div>
-        <Member
-          name="Christian Apostol"
-          position="VP of Projects"
-          linkedin="https://www.linkedin.com/in/capostol/"
-        />
+        <Carousel imageData={carouselImages} />
+        <div>
+          <h1 className="flex justify-center text-6xl font-bold py-8">The Team</h1>
+          <div className="flex flex-wrap">{memberComponents}</div>
+        </div>
       </div>
     </div>
   );
